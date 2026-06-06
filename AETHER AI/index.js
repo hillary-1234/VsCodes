ee50c4987c6bfe5b54274746a96de5629d75da4c
Hillary <nwangenehillary@gmail.com>
2026-06-06 16:59:18 +0100

    Adding JS

--- AETHER AI/index.js
+++ AETHER AI/index.js
@@ -1,28 +1,32 @@
 
 
+const wrapper = document.querySelector(".wrapper")
+const loginLink = document.querySelector(".login-link")
+const registerLink = document.querySelector(".register-link")
+const forgotLink = document.querySelector(".forgot-link")
+const btnPopup = document.querySelector(".btnLogin-popup")
+const iconClose = document.querySelector(".icon-close")
 
+registerLink.addEventListener("click", ()=> {
+    wrapper.classList.add("active")
+})
 
+loginLink.addEventListener("click", ()=> {
+    wrapper.classList.remove("active")
+})
 
+// // when you make the reset password page 
+// forgotLink.addEventListener("click", ()=> {
+//     wrapper.classList.add("active-popup")
+// })
 
+btnPopup.addEventListener("click", ()=> {
+    wrapper.classList.add("active-popup")
+})
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+iconClose.addEventListener("click", ()=> {
+    wrapper.classList.remove("active-popup")
+})
 
 
 
